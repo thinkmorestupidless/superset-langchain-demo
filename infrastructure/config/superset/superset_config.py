2f6039374bbd187761262adcdb519bc169b6b224
Nikolas Giannikopoulos <gianick@users.noreply.github.com>
2023-06-27 15:50:40 +0100

    Docker/infra wip

--- infrastructure/config/superset/superset_config.py
+++ infrastructure/config/superset/superset_config.py
@@ -1,12 +1,3 @@
-from dotenv import load_dotenv
-import os
-
-# load the environment variables from the .env file
-load_dotenv()
-
-# get the value of the MY_VAR environment variable
-SECRET_KEY = os.environ['SUPERSET_SECRET_KEY']
-
 FEATURE_FLAGS = {
   "ENABLE_TEMPLATE_PROCESSING": True,
 }
